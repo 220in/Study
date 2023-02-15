4dcf870318eaf11621c896ae86128426e0068f3c
이인영 <121835105+220in@users.noreply.github.com>
2023-02-15 20:55:01 +0900

    Add files via upload

---
+++ ex1115변수연산자/src/입출력/ex01입출력.java
@@ -0,0 +1,38 @@
+package 입출력;
+
+import java.util.Scanner;
+
+public class ex01입출력 {
+
+	public static void main(String[] args) {
+		// 글자 확대 단축기 : ctrl +
+		// 글자 축소 단축기 : ctrl -
+		// 코드를 한 줄 삭제 단축기 : ctrl d
+		// 코드 실행 단축기 : ctrl f11
+		// 출력 단축키 : syso 작성 후 ctrl space
+		// 자동 import 단축키 : ctrl shift o (scanner 사용시)
+		// 원하는 코드 복사 단축기 : ctrl art 화살표 위 or 아래
+		
+		// 1. 출력
+		System.out.println("Hello Would!");
+		System.out.println("이인영");
+		
+		// 2. 입력
+		// 2-1) 입력받는 도구 꺼내오기
+		Scanner sc = new Scanner(System.in);
+		// 개행(줄바꿈)을 없애고 싶다면 ln을 지워주기
+		System.out.print("글자를 입력하세요 >> ");
+		// 2-2) 입력받기
+		sc.next();
+
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
